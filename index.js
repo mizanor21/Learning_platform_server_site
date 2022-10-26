6246bcdf32a89ceac44ab8814da7d76ff7cbc9df
mizanor21 <mizanorrahman398@gmail.com>
2022-10-26 16:13:29 +0600

    json data file require

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+const categories = require('./data/categories.json')
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('API Running!');
