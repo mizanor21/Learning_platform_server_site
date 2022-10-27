bf701ead2129ca91c751e93048d174fb1294d31f
mizanor21 <mizanorrahman398@gmail.com>
2022-10-27 16:04:07 +0600

    details to jeson and details part added

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-const categories = require('./data/categories.json')
+const categories = require('./data/categories.json');
+const details = require('./data/details.json');
 
 app.use(cors());
 
@@ -13,6 +14,12 @@ app.get('/', (req, res) => {
 // courses part
 app.get('/catagories', (req, res) => {
     res.send(categories);
+})
+
+app.get('/details/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = details.find(n => n.course_id === id);
+    res.send(selectedCourse);
 })
 
 app.listen(port, () => {
